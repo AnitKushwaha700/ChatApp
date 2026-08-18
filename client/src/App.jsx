bc30520bdbb677276c8896625120fb0eb64a75b0
Anit Kushwaha <kushwahaanitak@gmail.com>
2026-08-18 23:41:49 +0530

    feat: enhance application structure with routing, authentication, and messaging features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import SiteHeader from "./components/SiteHeader";
+import Home from "./pages/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Header from "./components/SiteHeader";
-import Home from "./pages/Home";
-import Chat from "./pages/chat";
+import Chat from "./pages/Chat";
+import ContactUs from "./pages/Contactus";
+import UserDashboard from "./pages/UserDashboard";
 
-function App() {
+const App = () => {
+  const path = useLocation().pathname;
+  console.log(path);
+
   return (
-    <BrowserRouter>
-      <Header />
+    <>
+      <Toaster />
+      {/* {path !== "/chat" && <SiteHeader />} */}
+      <SiteHeader />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
